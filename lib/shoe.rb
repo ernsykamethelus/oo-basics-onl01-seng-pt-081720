ea9237ff1b5fc275dbb2ac2fd8d6b0321c8e3e99
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-05 03:09:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,22 +1,25 @@
-class Shoe
-  
-   attr_accessor :color, :size, :material,:condition
-   attr_reader :brand
-   
-   BRANDS = []
-  
+ class Shoe
+ 
+ attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+
+  BRANDS = []
+
   def initialize(brand)
-    @brands = brand
-     BRANDS << brand
-     BRANDS == BRANDS.uniq!
+    @brand = brand
+    BRANDS << brand
+    BRANDS == BRANDS.uniq!
+  end
 end
 
-def cobble
-  self.condition = "new"
-  puts "Your shoe is as good as new!"
-end
-end
 
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end	  
+
+end 
   
   
   
