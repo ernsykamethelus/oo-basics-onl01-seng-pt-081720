5f2676b030790eb7a81eec768412385620712217
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-05 02:44:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -22,9 +22,17 @@ def cobble
   puts "Your shoe is as good as new!"
 end
 
-
+shoe.condition
+shoe.cobble
 
 end
+
+# it 'makes the shoe\'s condition new' do
+#       shoe.condition = "old"
+#       shoe.cobble
+#       expect(shoe.condition).to eq("new")
+#     end
+#   end
 
 # class Book
 #   attr_accessor :author, :page_count, :genre
