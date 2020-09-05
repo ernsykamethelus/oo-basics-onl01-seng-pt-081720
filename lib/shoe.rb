445d669df26e42f9587e34670a302621fa4624fe
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-05 02:38:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,46 @@
-# Make your shoe class here!
+class Shoe
+   attr_accessor :brand, :color, :size, :material,:condition, :cobble
+   attr_reader :shoe
+  
+  def initialize(shoe)
+    @shoe= shoe
+end
+
+def shoe 
+  @shoe
+end
+
+def shoe_condition(new)
+  @new=new
+end
+
+def new
+  @new
+end
+
+def cobble
+  puts "Your shoe is as good as new!"
+end
+
+
+
+end
+
+# class Book
+#   attr_accessor :author, :page_count, :genre
+#   attr_reader :title
+  
+#   def initialize(title)
+#     @title= title
+# end
+
+
+# def title 
+#   @title
+# end
+
+# def turn_page
+#   puts "Flipping the page...wow, you read fast!"
+# end
+
+# end
